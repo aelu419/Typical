9c7bae8d959de218fe9dfbbbc679ca53deb875cd
aelu419 <alanrtao@outlook.com>
2021-04-13 21:47:06 -0400

    clean up project

--- Assets/Visuals/TexturePainter/Colorer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//generally meant to convert a float to a color
-public abstract class Colorer
-{
-    public abstract Color Fetch(float t);
-}
